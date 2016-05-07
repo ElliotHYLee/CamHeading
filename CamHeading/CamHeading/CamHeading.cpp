e8d1a654a696e47460a00b369b9c5620fa4fc37c
ElliotHYLee <hylee101001@gmail.com>
2016-05-06 22:26:13 -0400

    ll

---
+++ CamHeading/CamHeading/CamHeading.cpp
@@ -0,0 +1,14 @@
+// CamHeading.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+int main()
+{
+
+
+    return 0;
+}
+
